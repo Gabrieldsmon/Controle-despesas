36588767b9d13b71e21ac9b68d7a17558875201b
Gabrieldsmon <gabrieldsmon@gmail.com>
2025-06-02 20:30:25 -0300

    Adicionando porcentagem

--- Orçamento_pessoal/main.py
+++ Orçamento_pessoal/main.py
@@ -1,6 +1,14 @@
 from tkinter import *
 from tkinter import Tk, ttk
 from PIL import Image, ImageTk
+
+# Importando barra de progresso do tlinter
+from tkinter.ttk import Progressbar
+
+# Importando matplotlib
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 # ----------------------------------- Cores -----------------------------------------------
 
 co0 = "#000000"
@@ -46,7 +54,26 @@ app_logo = Label(frameCima, image=app_img, text=" Orçamento Pessoal", width=900
 app_logo.place(x=0, y=0)
 
 
+#---------------------------------- porcentagem ---------------------------------------------
+
+def porcentagem():
+    l_nome = Label(frameMeio, text="Porcentagem da Receita gasta", height=1, anchor=NW, font=('Verdana 12'), bg=co3, fg=co1)
+    l_nome.place(x=7, y=5)
+
+    style =ttk.Style()
+    style.theme_use('default')
+    style.configure("black.Horizontal.TProgressbar", background='#daed6b')
+    style.configure("TProgressbar", thickness=25)
+    
+    bar = Progressbar(frameMeio, length=180, style='black.Horizontal.TProgressbar')
+    bar.place(x=10, y=35)
+    bar['value'] = 50
+    valor = 50
+
+    l_porcentagem = Label(frameMeio, text="{:,.2f}%".format(valor), anchor=NW, font=('Verdana 12'), bg=co3, fg=co1)
+    l_porcentagem.place(x=200, y=35)
 
 
 
+porcentagem()
 janele.mainloop()
