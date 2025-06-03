d2453cefe3dd33ed53eea51da79c1f7e441e2aec
Gabrieldsmon <gabrieldsmon@gmail.com>
2025-06-02 21:20:00 -0300

    função para gráficos bars

--- Orçamento_pessoal/main.py
+++ Orçamento_pessoal/main.py
@@ -74,6 +74,54 @@ def porcentagem():
     l_porcentagem.place(x=200, y=35)
 
 
+#--------------------------- função para gráficos bars ----------------------------------
+def grafico_bar():
+    lista_categorias = ['Renda', 'Despesas', 'Saldo']
+    lista_valores = [3000, 2000, 6540]
+
+    figura = plt.figure(figsize=(4, 3.45), dpi=60)
+    ax = figura.add_subplot(111)
+    ax.autoscale(enable=True, axis='both', tight=None)
+
+    ax.bar(lista_categorias, lista_valores, color=colors, width=0.9)
+
+    for c, i in enumerate(ax.patches):
+        ax.text(i.get_x() - .001, i.get_height() + .5,
+                str("{:,.0f}".format(lista_valores[c])), 
+                fontsize=17, fontstyle='italic', 
+                verticalalignment='bottom', color='dimgrey')
+
+    ax.set_xticks(range(len(lista_categorias)))
+    ax.set_xticklabels(lista_categorias, fontsize=16)
+
+    ax.patch.set_facecolor(co1)
+    ax.spines['bottom'].set_color("#1A44CE")
+    ax.spines['bottom'].set_linewidth(1)
+    ax.spines['right'].set_linewidth(0)
+    ax.spines['top'].set_linewidth(0)
+    ax.spines['left'].set_color("#F50C0C")
+    ax.spines['left'].set_linewidth(1)
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.tick_params(bottom=False, left=False)
+    ax.set_axisbelow(True)
+    ax.yaxis.grid(False, color=co3)
+    ax.xaxis.grid(False)
+
+    canva = FigureCanvasTkAgg(figura, frameMeio)
+    canva.get_tk_widget().place(x=10, y=70)
+
+
+
+
 
 porcentagem()
+grafico_bar()
+
+
+
+
+
 janele.mainloop()
